3f405c31f6823ea74d430febe09ecd3e6741d5f8
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-01-06 00:12:11 +0530

    clean init

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import { headers } from "next/headers";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+import { auth } from "~/server/better-auth";
+import { getSession } from "~/server/better-auth/server";
+import { api, HydrateClient } from "~/trpc/server";
+
+export default async function Home() {
+  const session = await getSession();
+
+  if (session) {
+    void api.post.getLatest.prefetch();
+  }
+
+  return (
+    <HydrateClient>
+      <main>
+        hi
+      </main>
+    </HydrateClient>
+  );
+}
